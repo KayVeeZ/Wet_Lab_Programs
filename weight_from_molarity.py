87bd1d52a2b93d63e7e9f365f77ec80597fd74f0
KayVeeZ <125533246+KayVeeZ@users.noreply.github.com>
2023-03-03 20:45:50 +0530

    Update weight_from_molarity.py

--- weight_from_molarity.py
+++ weight_from_molarity.py
@@ -2,33 +2,33 @@
 """
 Created on Fri Mar  3 11:32:04 2023
 
-@author: Kshitij Vashisth
+@author: KayVeeZ
 """
-'''
-a = input (":")
-b = float(a)/50
-print(b)
-'''
-print("A wet lab program by ::KayVeez::\n")
+
+
+print("A wet lab program by ::KayVeeZ::")
 
 #Calculate required solute from molarity
-mol_m = float(input("Please input molar mass of compoound in grams/mol : ")) 
-mol = float(input("Please input molarity (in mM) of compound : "))
-vol = float(input("Please input volume of solvent in milliliters : "))
-weight =  ((mol/1000)*vol*mol_m)/1000
-print(weight,"grams")
-b = input("Do you want to continue? Y/N : ")
+
+b = "Y"
 
 while True:
-    if b=="Y":
+    if (b=="Y" or b=="y"):
         mol_m = float(input("Please input molar mass of compoound in grams/mol : ")) 
-        mol = float(input("Please input molarity of compound : "))
+        mol = float(input("Please input molarity (in mM) of compound : "))
         vol = float(input("Please input volume of solvent in milliliters : "))
-        weight =  (mol*vol*mol_m)/1000
-        print(weight,"grams")
+        weight =  ((mol/1000)*vol*mol_m)
+        print(weight,"mg")
+        c = input("Do you wish to convert this measurement to grams? Y/N ")
+        if (c=="Y" or c=="y"):
+            weight = weight/1000
+            print(weight, "grams")
+        else:
+            pass
+        b = input("Do you want to continue? Y/N : ")
     else:
         a = 'Thank You!'
         print(a)
         break
-b = input("Press Enter to exit...")
+d = input("Press Enter to exit....")
  
